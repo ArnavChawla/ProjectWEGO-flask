42b122fedb6725f6ec00c5df21943596b65e3c5c
Arnav Chawla <arnavchawla23@gmail.com>
2020-08-30 14:06:47 -0700

    Added basic funcitonality

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask, request
-
+from flask import Flask, request, render_template, session
+import os,io
 app = Flask(__name__)
-
-@app.route('/insert>')
+app.secret_key = "ProjectWEGO-SEAL"
+@app.route('/insert/<figure>')
 def insert(figure):
-    
+    f = open(os.getcwd()+"/xml/Figure{}-center.xml".format(figure),'r',encoding='utf8').read()
+    print(f)
+    return render_template('test.html',data=f)
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 5000, True)
